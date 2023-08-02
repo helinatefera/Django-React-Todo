863549e75c621b334e9f827092641db5263231ef
helinatefera <helinatefera1212@gmail.com>
2023-08-02 01:46:30 -0700

    first

--- proOne/proOne/settings.py
+++ proOne/proOne/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173/",
+    "http://127.0.0.1:8000/",
 ]
